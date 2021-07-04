3f19c5c33f9a5b8f294636f241c830909157b722
xdgoi <50510528+xdgoi@users.noreply.github.com>
2021-07-04 17:22:04 +0800

    Create challenge.py

---
+++ What are variables/challenge.py
@@ -0,0 +1,14 @@
+# Welcome to the challenge.py file!
+# This file will verify you know about variables!
+# To find out what your challenge is, then read on!
+
+
+
+# You must make the following file
+# print your favourite food but you
+# MUST use at least one variable to do it!
+# There are a few ays but try to figure out the easiest/best solution!
+# When you are done, go to the answer.py file to find out the answer!
+
+
+# Put your answer below:
