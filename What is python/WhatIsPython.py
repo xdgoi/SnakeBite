64ee0d42b1c159d401bdd215941fb82f58a5763f
xdgoi <50510528+xdgoi@users.noreply.github.com>
2021-07-04 16:33:29 +0800

    Create WhatIsPython.py

---
+++ What is python/WhatIsPython.py
@@ -0,0 +1,41 @@
+# What is python?
+
+# Python is a powerful programming language!
+# It can be used for many things and is easy to learn!
+# Down below is a piece of python code that prints out the text "Hello World!"
+
+print("Hello World!")
+
+# The above piece of code runs a command called print()
+# and inside the brackets is what you want it to print.
+# Notice how you must put quotation marks to specify that
+# it is a piece of text? You don't have to just print out text.
+# You can also print out variables, which I will get to in
+# a later LearnMod. Doing that would look like this:
+
+number = 6
+print(number)
+# The above code would print out the number 6.
+
+
+# These are very basic commands. Each command does something different.
+# You can even make your own commands which looks a bit like this:
+
+def Hello():
+    print("Hello!")
+
+Hello()
+# Notice that you have to define your command
+# before you can run it.
+
+
+# The def command (which is short for "define") specifies
+# that you want to make your own command. You then put the
+# name of the command and you MUST put brackets at the end.
+# You then type in what you want your custom command to do.
+# It must have an indent/four spaces.
+
+
+# So now I have shown you some code,
+# I want you to try and complete the challenge.py file.
+# Also, every python file has the .py file extension a bit like .txt, .exe etc.
