aa3381225da635c5970e860e6da00f43b1fff47a
xdgoi <50510528+xdgoi@users.noreply.github.com>
2021-07-04 16:33:23 +0800

    Create challenge.py
    jfgyugvh

---
+++ What is python/challenge.py
@@ -0,0 +1,21 @@
+# Hello! This is the challenge.py file!
+# By the way, make sure you have read the WhatIsPython.py
+# file or you might get stuck trying to complete these questions!
+
+
+# You must make the python code below print
+# out the words "Hello World!".
+
+text = "1234567890"
+
+print(text)
+
+
+# You can edit the code above as much as you like!
+# The only thing you have to do is make it say Hello World!
+# You can also look online if you're really stuck!
+
+
+# When you're done, then that means you have
+# finished this LearnMod! Now you can continue 
+# to the next one or play around with some python code!
