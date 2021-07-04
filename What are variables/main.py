2e14f03650ca5517afcfa098b7b11659591daa2e
xdgoi <50510528+xdgoi@users.noreply.github.com>
2021-07-04 17:22:07 +0800

    Create main.py

---
+++ What are variables/main.py
@@ -0,0 +1,42 @@
+# In my last LearnMod, I mentioned variables.
+# Variables are like containers. They have a name and they have a value.
+# They can be defined/made like this:
+
+greeting = "Hello! How are you?"
+
+# The name of the variable in this example is
+# greeting. It's value is "Hello! How are you?".
+# You can print variables using the print() command
+# which we talked about in the What is Python? LearnMod.
+# It would look like this:
+
+FavouriteFood = "Pizza"
+
+print(FavouriteFood)
+
+# The value of the variable is Pizza
+# So when we print out the variable, it will print its value.
+
+
+# We can also join values also
+# known as concatenation. It looks like this:
+
+schoolOpinion = "boring."
+print("School is really " + schoolOpinion)
+
+# As you can tell, we are printing out "School is really " and then our value.
+# There is also another way that you can accomplish the same thing:
+# It looks like this:
+
+videoGame = "Minecraft"
+print(f"I love {videoGame}")
+
+# The above example is called an fstring because it
+# has the letter f next to it.
+
+
+
+
+# This is part 1 of What are variables! Make sure to
+# do part 2 and to do the challenge.py to verify you
+# know everything that we covered!
